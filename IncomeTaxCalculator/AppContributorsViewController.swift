147582ce7be5733dd08a9d462460b434b4fe821b
u6625166 <u6625166@anu.edu.au>
2018-11-17 13:29:26 +1100

    Created new view controller and its xib file

---
+++ IncomeTaxCalculator/AppContributorsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AppContributorsViewController.swift
+//  IncomeTaxCalculator
+//
+//  Created by 杨偲 on 17/11/18.
+//  Copyright © 2018 Zhiyu Ding. All rights reserved.
+//
+
+import UIKit
+
+class AppContributorsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
