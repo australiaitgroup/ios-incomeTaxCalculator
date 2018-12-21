40b1650a8f90c5f12f8b7d048c38758ab33be616
u6625166 <4342315yc@gmail.com>
2018-12-21 15:48:07 +1100

    Reset all the components

--- IncomeTaxCalculator/AppContributorsViewController.swift
+++ IncomeTaxCalculator/AppContributorsViewController.swift
@@ -11,112 +11,155 @@ import UIKit
 class AppContributorsViewController: UIViewController {
     
     
-    var graphics = UILabel(frame: CGRect(x: 16, y: 108, width: 100, height: 36))
-    var developers = UILabel(frame: CGRect(x: 16, y: 262, width: 130, height: 36))
-    var boss = UILabel(frame: CGRect(x: 16, y: 513, width: 130, height: 36))
     
-    var designer = UILabel(frame: CGRect(x: 16, y: 177, width: 79, height: 29))
-    var developerOne = UILabel(frame: CGRect(x: 16, y: 340, width: 153, height: 29))
-    var developerTwo = UILabel(frame: CGRect(x: 16, y: 424, width: 92, height: 29))
-    var manager = UILabel(frame: CGRect(x: 16, y: 596, width: 99, height: 29))
+    var designer = UILabel(frame: CGRect(x: 16, y: 177, width: 120, height: 35))
+    var developerOne = UILabel(frame: CGRect(x: 16, y: 340, width: 160, height: 35))
+    var developerTwo = UILabel(frame: CGRect(x: 16, y: 424, width: 160, height: 35))
+    var marketing = UILabel(frame: CGRect(x: 16, y: 596, width: 120, height: 35))
+    var coOrdinator = UILabel(frame: CGRect(x: 16, y: 596, width: 160, height: 35))
+    
+    var MichealLinLiu = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 29))
+    var CaiYang = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 29))
+    var SissyZhou = UIButton(frame: CGRect(x: 0, y: 0, width: 130, height: 29))
+    var Lightman = UIButton(frame: CGRect(x: 0, y: 0, width: 130, height: 29))
+    var MichealDing = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 29))
+    var returnButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
     
     let titleFont = UIFont(name: "ChalkboardSE-Bold", size: 25)
-    let contentFont = UIFont(name: "Chalkboard SE", size: 20)
-    let color = UIColor.black
+    let contentFont = UIFont(name: "Chalkboard SE", size: 25)
+    let color = UIColor.white
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print(self.view.bounds.height)
+        print(self.view.center.y)
+        print(self.view.bounds.midY)
+        print(UIScreen.main.bounds.size)
+        print(UIScreen.main.bounds.size.width)
         createGradientLayer()
         setLabels()
-        addLabels()
+        setButtons()
+        addLabelsAndButtons()
 
     }
 
 
     // This function is used to set the actual content of each label
     func setLabels(){
-        graphics.text = "Graphic";
-        graphics.font = titleFont
-        graphics.textColor = color
-        graphics.center.x = self.view.center.x
         
-        developers.text = "Developers";
-        developers.font = titleFont
-        developers.textColor = color
-        developers.center.x = self.view.center.x
-        
-        boss.text = "Marketing";
-        boss.font = titleFont
-        boss.textColor = color
-        boss.center.x = self.view.center.x
-        
-        designer.text = "• Sissy";
-        designer.font = contentFont
+        let distance = UIScreen.main.bounds.size.height / 15
+        let xCenterPos = UIScreen.main.bounds.size.width/2
+        let yCenterPos = UIScreen.main.bounds.size.height/2
+ 
+        designer.text = "Designer"
+        designer.font = titleFont
         designer.textColor = color
-        designer.center.x = self.view.center.x
+        designer.center.x = xCenterPos/2
+        designer.center.y = yCenterPos - distance
         
-        developerOne.text = "• Micheal Lin Liu";
-        developerOne.font = contentFont
+        developerOne.text = "Developer"
+        developerOne.font = titleFont
         developerOne.textColor = color
-        developerOne.center.x = self.view.center.x
-        
-        developerTwo.text = "• Cai Yang";
-        developerTwo.font = contentFont
+        developerOne.center.x = xCenterPos/2
+        developerOne.center.y = yCenterPos - distance * 3
+       
+        developerTwo.text = "Developer"
+        developerTwo.font = titleFont
         developerTwo.textColor = color
-        developerTwo.center.x = self.view.center.x
+        developerTwo.center.x = xCenterPos/1.7
+        developerTwo.center.y = yCenterPos - distance * 2
         
-        manager.text = "• Lightman";
-        manager.font = contentFont
-        manager.textColor = color
-        manager.center.x = self.view.center.x
+        marketing.text = "Marketing"
+        marketing.font = titleFont
+        marketing.textColor = color
+        marketing.center.x = xCenterPos/2
+        marketing.center.y = yCenterPos
+        
+        coOrdinator.text = "Co-ordinator"
+        coOrdinator.font = titleFont
+        coOrdinator.textColor = color
+        coOrdinator.center.x = xCenterPos/2
+        coOrdinator.center.y = yCenterPos + distance
+        
+       
+    
     }
     
     // This function is used to add all the labels to the current view
-    func addLabels(){
-        self.view.addSubview(graphics)
-        self.view.addSubview(developers)
-        self.view.addSubview(boss)
+    func addLabelsAndButtons(){
+        
+        self.view.addSubview(marketing)
         self.view.addSubview(designer)
         self.view.addSubview(developerOne)
         self.view.addSubview(developerTwo)
-        self.view.addSubview(manager)
+        self.view.addSubview(coOrdinator)
+
+        self.view.addSubview(MichealDing)
+        self.view.addSubview(CaiYang)
+        self.view.addSubview(MichealLinLiu)
+        self.view.addSubview(Lightman)
+        self.view.addSubview(SissyZhou)
+        self.view.addSubview(returnButton)
 
     }
     
+    func setButtons(){
+        
+        let distance = UIScreen.main.bounds.size.height / 15
+        let xCenterPos = UIScreen.main.bounds.size.width/2
+        let yCenterPos = UIScreen.main.bounds.size.height/2
+//        let distance = self.view.bounds.height / 15
+        
+        MichealLinLiu.setTitle("Micheal Lin Liu", for: .normal)
+        MichealLinLiu.setTitleColor(color, for: .normal)
+        MichealLinLiu.titleLabel?.font = contentFont
+        MichealLinLiu.center.x = xCenterPos * 1.3
+        MichealLinLiu.center.y = yCenterPos - distance * 3
+        MichealLinLiu.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        CaiYang.setTitle("Cai Yang", for: .normal)
+        CaiYang.setTitleColor(color, for: .normal)
+        CaiYang.titleLabel?.font = contentFont
+        CaiYang.center.x = xCenterPos * 1.4
+        CaiYang.center.y = yCenterPos - distance * 2
+        CaiYang.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        SissyZhou.setTitle("Sissy Zhou", for: .normal)
+        SissyZhou.setTitleColor(color, for: .normal)
+        SissyZhou.titleLabel?.font = contentFont
+        SissyZhou.center.x = xCenterPos * 1.4
+        SissyZhou.center.y = yCenterPos - distance
+        SissyZhou.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        Lightman.setTitle("Lightman", for: .normal)
+        Lightman.setTitleColor(color, for: .normal)
+        Lightman.titleLabel?.font = contentFont
+        Lightman.center.x = xCenterPos * 1.4
+        Lightman.center.y = yCenterPos
+        Lightman.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        MichealDing.setTitle("Micheal Ding", for: .normal)
+        MichealDing.setTitleColor(color, for: .normal)
+        MichealDing.titleLabel?.font = contentFont
+        MichealDing.center.x = xCenterPos * 1.4
+        MichealDing.center.y = yCenterPos + distance
+        MichealDing.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        returnButton.setImage(#imageLiteral(resourceName: "ButtonImage.png"), for: .normal)
+        returnButton.center.x = xCenterPos * 1.45
+        returnButton.center.y = yCenterPos + distance * (4 * distance / 60)
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        UIApplication.shared.openURL(URL(string: "https://www.google.com")!)
+    }
 
-//    // This function is used to set up the navigation bar along with two bar button items
-//    func setNavigationBar(){
-//        let myNav = UINavigationBar(frame: CGRect(x: 0, y: 30, width: self.view.frame.width, height: 44))
-//        myNav.barTintColor = UIColor.purple
-//        myNav.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.purple]
-//        self.view.addSubview(myNav)
-//        let backItem: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .reply, target: self, action: #selector(Progress.cancel))
-//        backItem.tintColor = UIColor(red: 0.02745, green: 0.09020, blue: 0.25490, alpha: 1)
-//
-////        let moreItem = UIBarButtonItem(title: "More", style: .plain, target: self, action: Selector(("More")))
-////
-////        moreItem.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-//        let navigItem: UINavigationItem = UINavigationItem(title: "Contributors")
-//
-//        //navigItem.rightBarButtonItem = moreItem
-//        navigItem.leftBarButtonItem = backItem
-//        myNav.items = [navigItem]
-//
-//        let top = NSLayoutConstraint(item: myNav, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 40)
-//        let leading = NSLayoutConstraint(item: myNav, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 0)
-//        let trailing = NSLayoutConstraint(item: myNav, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: 0)
-//        view.addConstraints([top, leading, trailing])
-//
-//        myNav.translatesAutoresizingMaskIntoConstraints = false
-//    }
-//
     func createGradientLayer() {
         var gradientLayer: CAGradientLayer!
         
         gradientLayer = CAGradientLayer()
         
-        gradientLayer.frame = self.view.bounds
+        gradientLayer.frame = UIScreen.main.bounds
         
         let startColor = UIColor(red: 0.02745, green: 0.09020, blue: 0.25490, alpha: 1).cgColor
         let endColor = UIColor(red: 0.25098, green: 0.196078, blue: 0.54902, alpha: 1).cgColor
